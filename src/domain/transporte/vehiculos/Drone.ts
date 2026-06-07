2d0ce665141d84eb66ff80f74d7e6156c5b16eec
anaceli01 <karlaanaceli.miranda@alumnos.ulagos.cl>
2026-06-07 18:22:51 -0400

    Se modificó el cálculo de costo por despacho para cada vehiculo.

--- src/domain/transporte/vehiculos/Drone.ts
+++ src/domain/transporte/vehiculos/Drone.ts
@@ -1,9 +1,15 @@
 import { EstrategiaTransporte } from '../EstrategiaTransporte'; // (../) <- ESTO ES PARA CUANDO EL ARCHIVO ESTÁ FUERA DEL ACTUAL
+import {VEHICULOS} from '../../../data/MockDataVehiculos';
+import mockData from '../../../data/MockData.json';
 
 export class Drone implements EstrategiaTransporte {
-  calcularCosto(distanciaKm: number, pesoKg: number): number {
-    return 15000 + (distanciaKm * 300) + (pesoKg * 500);
-    //FÓRMULA COSTO TOTAL DRONE = COSTO FIJO: $15.000 + $300*DISTANCIA + $500*PESO
+  calcularCosto(distanciaKm: number): number {
+
+    const costoBase = VEHICULOS.dron.costoBase;
+    const costoPorKm = VEHICULOS.dron.costoPorKm;
+
+    return costoBase + (distanciaKm * costoPorKm); ;
+    //FÓRMULA COSTO TOTAL DRONE
   }
   calcularTiempoEstimado(distanciaKm: number): string {
     return `${Math.max(1, Math.ceil(distanciaKm / 120))} hrs`; // LAS COMILLAS INVERTIDAS (``) PERMITEN MEZCLAR TEXTO CON VARIABLES
