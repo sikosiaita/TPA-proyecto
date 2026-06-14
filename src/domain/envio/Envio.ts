f4d52ce88d7332c8a95b31b5fe3359e89e158caf
Yerko Flores <yerkonicolas.flores@alumnos.ulagos.cl>
2026-06-14 00:23:06 -0400

    Primera estructura de EstadoEnvio y Envio

--- src/domain/envio/Envio.ts
+++ src/domain/envio/Envio.ts
@@ -1,1 +1,26 @@
-export {};
+import { EstadoEnvio } from './EstadoEnvio';
+
+export class Envio {
+  constructor(
+    public readonly id: string,
+    public readonly origen: string,
+    public readonly destino: string,
+    public readonly transporte: string,
+    public estado: EstadoEnvio,
+    public tiempoEstimado: string | null = null,
+  ) {}
+
+  estaActivo(): boolean {
+    return this.estado === EstadoEnvio.EN_PREPARACION
+        || this.estado === EstadoEnvio.EN_TRANSITO;
+  }
+
+  getColorEstado(): string {
+    switch (this.estado) {
+      case EstadoEnvio.EN_PREPARACION: return '#FFAEC1';
+      case EstadoEnvio.EN_TRANSITO:    return '#ED93B1';
+      case EstadoEnvio.ENTREGADO:      return '#4ade80';
+      case EstadoEnvio.FALLIDO:        return '#f87171';
+    }
+  }
+}
