5cfc55d0ecc15c7fb0b5860e2c84ad7315894677
Pia Rojas <piavalentina.rojas@alumnos.ulagos.cl>
2026-06-27 20:43:08 -0400

    se vuelven a importar archivos, ya que hubo problemas de compatibilidad con leaflet, se busca usar metodo alternativo

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Layout from '../components/Layout';
 import mockData from '../data/MockData.json';
-import { MapContainer, TileLayer, Marker, Popup, Polyline, useMap } from 'react-leaflet';
-import L from 'leaflet';
 import { Package, Truck, CheckCircle, Clock } from 'lucide-react';
-
-
-
